ea64eb339785fc54ff63f88839a63bf8c3e30074
ElkkuTee <you@example.com>
2025-09-16 16:19:49 +0300

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+console.log('main.jsx is loading...')
+
+const App = () => {
+  console.log('App component is rendering...')
+  return <div><h1>Hello World - React is working!</h1></div>
+}
+
+console.log('About to find root element...')
+const rootElement = document.getElementById('root')
+console.log('Root element:', rootElement)
+
+if (rootElement) {
+  const root = createRoot(rootElement)
+  console.log('Root created, about to render...')
+  root.render(<App />)
+  console.log('Render called!')
+} else {
+  console.error('Could not find root element!')
+}
--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,11 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    sourcemap: false,
     rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  server: {
-    port: 3000,
-  },
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
