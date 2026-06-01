095732d4022de026b23a3fe6274fdec8365159ea
ElkkuTee <102071354+ElkkuTee@users.noreply.github.com>
2026-06-02 00:20:11 +0300

    oops forgot to save (this one is deadass pointless but i still want to push cuz pregnant code)

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
     siteName: 'PARKOUR Reborn',
     title: siteTitle,
     description: siteDescription,
-    images: [{ url: '/embed.png', width: 1200, height: 630, alt: 'PARKOUR Reborn' }],
+    images: [{ url: '/embed.png', width: 760, height: 399, alt: 'PARKOUR Reborn' }],
   },
   twitter: {
     card: 'summary_large_image',
