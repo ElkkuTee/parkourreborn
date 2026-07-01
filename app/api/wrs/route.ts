8ace93046fe906f6c168004072dbd141a05e4c6d
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2026-07-01 13:00:09 -0500

    Update WRS API URL to new endpoint

--- app/api/wrs/route.ts
+++ app/api/wrs/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-const wrsURL = 'https://wasans.tully.sh/api/wrs';
+const wrsURL = 'https://wasans.tully.sh/v1/records/world';
 
 export const dynamic = 'force-dynamic';
 
